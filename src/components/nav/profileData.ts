a72622168c593532e4b868170c6b847671f813f6
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-08-21 09:29:43 +0100

    Aug 21th, 24 > I optimize UI codebase using react's *Seperation Of Concern* concept > I modified some error messages on the backend > I change up the folder structure of both ends, since it's causing confusion > Progress (%) - 68 I made some breakthrough on the '.map is not a function error i've been getting Finished off section page, now started the notepage Refactoring starts...

---
+++ src/components/nav/profileData.ts
@@ -0,0 +1,32 @@
+import { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z } from '../..';
+
+export let profileImages = {
+   A,
+   B,
+   C,
+   D,
+   E,
+   F,
+   G,
+   H,
+   I,
+   J,
+   K,
+   L,
+   M,
+   N,
+   O,
+   P,
+   Q,
+   R,
+   S,
+   T,
+   U,
+   V,
+   W,
+   X,
+   Y,
+   Z,
+};
+
+export const profileBackgroundColor = ['green', 'Blue', 'Purple', 'Orange', 'Red', 'Yello', 'Crimson'][Math.floor(Math.random() * 7)];
