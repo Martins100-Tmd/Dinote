5a89ff59c6a77ae39b528f6545d63f61b15e4e94
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-08-29 08:03:18 +0100

    Aug 29th, 2024

--- bsrc/controller/put.ts
+++ bsrc/controller/put.ts
@@ -2,7 +2,7 @@ import { Response, Request } from 'express';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 //:PAGE
-export const createANewPage = async function (req: Request, res: Response) {
+export const updateAPage = async function (req: Request, res: Response) {
    const { userId, title, content, sectionId } = req.body;
    const id = req.params.id;
    if (userId) {
@@ -11,7 +11,17 @@ export const createANewPage = async function (req: Request, res: Response) {
       if (user && section) {
          const updatePage = await prisma.page.update({
             where: { id },
-         })
-      }
+            data: {
+               title,
+               content,
+            },
+         });
+         if (updatePage) res.status(200).json({ success: true, msg: 'Page successfully updated' });
+         else res.status(401).json({ success: false, msg: 'Error updating page' });
+      } else
+         res.status(401).json({
+            success: false,
+            msg: 'Make sure user and section are valid',
+         });
    }
 };
--- bsrc/router/put.ts
+++ bsrc/router/put.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import userAuthMiddleWare from '../middleware/userId';
+import { updateAPage } from '../controller/put';
 
 export const putRouter = express.Router();
 
 putRouter.put('/onenote/:id', userAuthMiddleWare);
 putRouter.put('/onesect/:id', userAuthMiddleWare);
-putRouter.put('/onepage/:id', userAuthMiddleWare);
+putRouter.put('/onepage/:id', userAuthMiddleWare, updateAPage);
