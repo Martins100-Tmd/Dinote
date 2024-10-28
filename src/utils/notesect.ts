b7534807c7c0d4594be0d2a093ee80c5b299a2fd
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-10-28 08:39:50 +0100

    Miraculously Change up the whole app to be working like intended

--- src/utils/notesect.ts
+++ src/utils/notesect.ts
@@ -4,7 +4,6 @@ import useGlobalReducer from './hooks/reducer';
 export function addNoteLogicFn(notename: string, addNoteMutation: UseMutationResult<any, Error, string, unknown>) {
    let { addNote } = useGlobalReducer();
    const regExpNote = /^[a-zA-Z0-9 ]{3,20}/;
-   console.log(notename, regExpNote.test(notename));
    if (notename && regExpNote.test(notename)) {
       addNoteMutation.mutate(notename, {
          onSuccess() {
