d4cfa6936c355f866ed12c26550b93204974948c
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-10-29 12:53:08 +0100

    Optimizing...

--- src/components/main/Layout.tsx
+++ src/components/main/Layout.tsx
@@ -2,20 +2,13 @@ import NoteSection from './Section/section';
 import NotePage from './Page/Page';
 import { useContext, useEffect, useState } from 'react';
 import createNoteState from '../state/context';
+import { NoteStore, NoteStoreState } from '../state/_note';
 
 const NoteLayout = function () {
-   let {
-      state: { noteObj },
-   } = useContext(createNoteState);
-   let [id, setId] = useState('');
-
-   useEffect(() => {
-      if (noteObj) setId(noteObj.id);
-   }, [noteObj]);
-
+   let noteid = NoteStore((s: NoteStoreState) => s.currNoteId);
    return (
       <section className='flex flex-row w-full h-[93%] bg-[rgba(54,54,54,0.9)]'>
-         <NoteSection id={id} />
+         <NoteSection id={noteid} />
          <NotePage />
       </section>
    );
