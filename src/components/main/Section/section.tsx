56da4b268a4d725e70d8a1dd950dbeb09b7b7a44
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-10-29 12:19:17 +0100

    Optimizing...

--- src/components/main/Section/section.tsx
+++ src/components/main/Section/section.tsx
@@ -5,7 +5,7 @@ import { addSection } from './op';
 
 const NoteSection = ({ id }: { id: string }) => {
    const queryClient = useQueryClient();
-   let [state, setstate] = useState({ createSection: false });
+   let [state, setstate] = useState(false);
    let [sectVal, setSectVal] = useState('');
 
    let addSectMutation = useMutation({
@@ -20,10 +20,10 @@ const NoteSection = ({ id }: { id: string }) => {
    });
 
    return (
-      <section className='flex flex-col items-start w-[70%] sm:w-full border-r border-[#222222] justify-between h-full p-2'>
+      <section className='flex flex-col items-start sm:w-[70%] w-[90%] border-r border-[#222222] justify-between h-full p-2'>
          <section className='flex flex-col gap-2 items-start w-full'>
             <SectionContainer id={id} />
-            <div className={`${state.createSection ? 'flex' : 'hidden'} flex-row items-center w-full p-2`}>
+            <div className={`${state ? 'flex' : 'hidden'} flex-row items-center w-full p-2`}>
                <input
                   onInput={(e) => {
                      const target = e.target as HTMLInputElement;
@@ -32,26 +32,20 @@ const NoteSection = ({ id }: { id: string }) => {
                   onBlur={(e) => {
                      if (sectVal && id) {
                         addSectMutation.mutate({ title: sectVal, noteId: id });
-                        setstate((prev) => ({ ...prev, createSection: false }));
+                        setstate(false);
                         e.target.value = '';
                      }
                   }}
                   type='text'
                   className='border border-gray-300 bg-transparent text-white outline-none p-2 font-redit text-base'
                />
-               <i
-                  onClick={() => setstate((prev) => ({ ...prev, createSection: false }))}
-                  className='material-icons cursor-pointer text-xl text-gray-300'
-               >
+               <i onClick={() => setstate(false)} className='material-icons cursor-pointer text-xl text-gray-300'>
                   clear
                </i>
             </div>
          </section>
          <section className='justify-end flex'>
-            <div
-               className='flex flex-row items-center cursor-pointer'
-               onClick={() => setstate((prev) => ({ ...prev, createSection: state.createSection ? false : true }))}
-            >
+            <div className='flex flex-row items-center cursor-pointer' onClick={() => setstate(!state)}>
                <i className='material-icons text-2xl text-emerald-200 font-thin'>add</i>
                <p className='font-sand text-sm text-left text-emerald-100 font-medium'>Add Section</p>
             </div>
